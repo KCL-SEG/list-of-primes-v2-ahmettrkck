89c77922403fd3ddb40b63ab7710078f63a3492c
Ahmet Kucuk <ahm3ttarik@gmail.com>
2023-10-01 19:19:05 +0100

    add exception when primes method called with an integer less than 1

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <=0:
+        raise ValueError("Number of primes must be greater than zero.")
+
     list = []
+    candidate = 2
+    while len(list) < number_of_primes:
+        is_prime = True
+        for divisor in range(2,int(candidate**0.5) + 1):
+            if candidate % divisor == 0:
+                is_prime = False
+                break
+        if is_prime:
+            list.append(candidate)
+        candidate += 1
     return list
